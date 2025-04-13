4293780a723ee90e76168e2c4cefa8a6041862fe
ZabakJL <jpmogollon@gmail.com>
2025-04-13 16:42:15 -0500

    Inicial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 13 16:28:42 2025
+
+@author: jpmog
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="nn_visualizer",
+    version="0.1",
+    description="Feedforward Neural Network Visualizer for Keras models",
+    author="TuNombre",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "tensorflow"
+    ],
+)
